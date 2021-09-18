2c911081a0cef55e4824e17f9d5abcbffa1debac
zhangsan <zhangsan@gmail.com>
2021-09-19 04:01:14 +0800

    auto push

--- autopush.sh
+++ autopush.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-#github地址，ssh方式
+#根据需要创建定时任务
+# 参考命令：crontab -e
+# 8 8 * * * root /root/autopush.sh &2>&1 >>/root/autopush.log
+#
+
+#git的参数配置，邮箱，作者(改)
+git config --global user.email "zhangsan@gmail.com"
+git config --global user.name "zhangsan"
+
+#github地址，ssh方式(改)
 github_url='git@github.com:alice1530/FirstOne.git'
 
-#文件存放绝对位置， /xxx/bb/ccc
-file_path='/root/zzzautopush/update.sh'
+#要推送的文件路径(改)
+#文件或文件夹，存放绝对位置， /xxx/bb/ccc
+file_path='/root/zzzautopush/autopush.sh'
 
-###获取项目名称
+###获取项目名称(不用动)
 tmp1="${github_url##*/}"
 repository_name="${tmp1%%.*}"
 
-###本地仓库位置
+###本地仓库位置(不用动)
 local_path="$PWD/$repository_name/"
 
-###时间格式
+###时间格式(不用动)
 DATA_TIME=`date +'%Y年%m月%d日 %H:%M:%S'`
 
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
 
-#echo $tmp1
-#echo $local_path
 
 if [[ ${github_url} =~ 'https://' ]];then
   echo "---请使用ssh方式的git---"
   exit 1
 fi
+
+
 
 push_code(){
   
@@ -42,11 +50,14 @@ push_code(){
 
   if [ $? != 0 ];then
       echo "代码推送失败！"
+      exit 1
   else
       echo "代码推送成功！"
   fi
 
 }
+
+
 
 if [ ! -d "$local_path" ];then
   echo "文件夹不存在,开始克隆..."
@@ -54,6 +65,7 @@ if [ ! -d "$local_path" ];then
   #mkdir -p $local_path
   if [ $? != 0 ];then
       echo "克隆失败！"
+      exit 1
   else
       echo "克隆成功！开始执行推送操作..."
       push_code
@@ -65,21 +77,4 @@ else
 fi
 
 
-#exit 0
-
-#DATA_TIME=`date +'%Y年%m月%d日 %H:%M:%S'`
-
-
-
-
-
-#cd /ql/config/wskey_sigm_list/
-
-#git add .
-#sleep 1
-
-#git commit -m  "auto_commit $dt"
-#sleep 1
-
-#git push
-
+exit 0
